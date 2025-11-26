cdd497b5e5febaa9e9b1079db3f4329bd75c2be7
Gerry Dassi <dassige@gmail.com>
2025-11-26 15:06:53 +1300

    updates

--- main.js
+++ main.js
@@ -1,75 +1,22 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
-const { initializeWhatsAppClient, sendMessage } = require('./whatsapp.js');
+const https = require('https');
 
-const members = [
-    { "name": "QFF Paulin, N M", "mobile": "64272061827", "skills": [] },
-    { "name": "CFO Sweeting-Shaw, P J", "mobile": "64204721553", "skills": [] },
-    { "name": "SFF Fouche, C D", "mobile": "6421646254", "skills": [] },
-    { "name": "DCFO Milne, D S", "mobile": "64225154321", "skills": [] },
-    { "name": "RFF Collins, S B", "mobile": "6421508980", "skills": [] },
-    { "name": "FF Mason, J D", "mobile": "6421878400", "skills": [] },
-    { "name": "FF Godfrey, D C", "mobile": "64212729289", "skills": [] },
-    { "name": "FF Mollier-Alexander, C", "mobile": "6421719328", "skills": [] },
-    { "name": "SFF Reilly, L B", "mobile": "64272118327", "skills": [] },
-    { "name": "FF Fitzpatrick, R", "mobile": "64221877318", "skills": [] },
-    { "name": "QFF Sparrow, M J", "mobile": "64273735998", "skills": [] },
-    { "name": "OS-FF Iszard, A M", "mobile": "64211476282", "skills": [] },
-    { "name": "SO Laloli, B J", "mobile": "64276002202", "skills": [] },
-    { "name": "FF Hunter, W R", "mobile": "64220594566", "skills": [] },
-    { "name": "RFF Hyde, C L", "mobile": "642102898974", "skills": [] },
-    { "name": "RFF Walmsley, B G", "mobile": "642041601077", "skills": [] },
-    { "name": "FF Crawley, P J", "mobile": "6421483114", "skills": [] },
-    { "name": "OS-FF Sweeting-Shaw, B", "mobile": "64212562563", "skills": [] },
-    { "name": "OS-FF Partington, A C", "mobile": "64212107129", "skills": [] }
-]
-
-const skillUrls = [
-    { "name": "OI (IS1) - Operational Safety (C)", "url": "https://forms.office.com/Pages/ResponsePage.aspx?id=pX8_-1gNx0GqJPcLmyIgxnmVztZMX6FPrvJPWanW91RUM0VBMVBDU1VXUTI2RlZNSDM2RDZPVFlUMS4u" },
-    { "name": "OI (H6-1) - Bulk Flammable Gases", "url": "https://forms.office.com/Pages/ResponsePage.aspx?id=pX8_-1gNx0GqJPcLmyIgxnmVztZMX6FPrvJPWanW91RUM0paSDJGWU45NFhFWllLSFROQzZBS0k4TC4u" },
-    { "name": "OI (H5) - Bulk Flammable Liquids", "url": "https://forms.office.com/Pages/ResponsePage.aspx?id=pX8_-1gNx0GqJPcLmyIgxnmVztZMX6FPrvJPWanW91RUQk1ROVVLV0U2T0xMRlJaRkpDQkhMQUtaMy4u" },
-    { "name": "OI (FL2-1) - Use of FENZ Operational Vehicles", "url": "https://forms.office.com/Pages/ResponsePage.aspx?id=pX8_-1gNx0GqJPcLmyIgxnmVztZMX6FPrvJPWanW91RUMlBCS1ZIRUk3QVM0SElPQVZCN1oxWTdBNi4u" },
-    { "name": "OI (E3-2) - Breathing Apparatus (C)", "url": "https://forms.office.com/Pages/ResponsePage.aspx?id=pX8_-1gNx0GqJPcLmyIgxnmVztZMX6FPrvJPWanW91RUNzJMMlQ0Vk1MQkdYWjhQQlFFUEg5SjdDSC4u" },
-    { "name": "OI (G9) - Salvage", "url": "https://forms.office.com/Pages/ResponsePage.aspx?id=pX8_-1gNx0GqJPcLmyIgxnmVztZMX6FPrvJPWanW91RUN0FQN0lJODMxT1NaSVIzSThMUzZYTkxBNy4u" },
-    { "name": "OSH - Safe Person Concept (C)", "url": "https://forms.office.com/Pages/ResponsePage.aspx?id=pX8_-1gNx0GqJPcLmyIgxnmVztZMX6FPrvJPWanW91RURFlDMUY5WURSN0tVMDROTVJYTkMxVTdCNS4u" },
-    { "name": "OI (H1-H6) - Hazardous Materials", "url": "https://forms.office.com/Pages/ResponsePage.aspx?id=pX8_-1gNx0GqJPcLmyIgxnmVztZMX6FPrvJPWanW91RUM1FYRlZDV1FVRUpQNks4WDZDVThKOFEzWi4u" },
-    { "name": "OI (H6-2) - Portable gas cylinders and pressurised vessels", "url": "https://forms.office.com/Pages/ResponsePage.aspx?id=pX8_-1gNx0GqJPcLmyIgxnmVztZMX6FPrvJPWanW91RUN0VDUjNaOTNTWU03WEhYMUJaS0I5NUJXOC4u" },
-    { "name": "OI (G2-1) - Emergency Medical Support", "url": "https://forms.office.com/Pages/ResponsePage.aspx?id=pX8_-1gNx0GqJPcLmyIgxnmVztZMX6FPrvJPWanW91RUMEg2UUQ0VkxYME5RNEUwUTNLQ0ZBSVRNVy4u"},
-    { "name": "OI (IS3) - Working Near Roadways (C)", "url": "https://forms.office.com/Pages/ResponsePage.aspx?id=pX8_-1gNx0GqJPcLmyIgxnmVztZMX6FPrvJPWanW91RUNjdIV0lROTRaTEhFTUFFVUdGQjlSQUhRNC4u" },
-    { "name": "OI (IS4) - Working Near Electrical Hazards (C)", "url": "https://forms.office.com/Pages/ResponsePage.aspx?id=pX8_-1gNx0GqJPcLmyIgxnmVztZMX6FPrvJPWanW91RUQVFUUTJBRkdSNUxSNFBXUjQzWDlCVlkxVC4u" },
-    { "name": "OI (IS9) - Civil Disturbances", "url": "https://forms.office.com/Pages/ResponsePage.aspx?id=pX8_-1gNx0GqJPcLmyIgxnmVztZMX6FPrvJPWanW91RUOExPWlhUSUZHNjQzUFkwVlJFWEJTSE5DWC4u" },
-    { "name": "OI (H7-1) - Clandestine Labs", "url": "https://forms.office.com/Pages/ResponsePage.aspx?id=pX8_-1gNx0GqJPcLmyIgxnmVztZMX6FPrvJPWanW91RUN0RWQjRTU0FXQTFVTDRTWFJOVFRWTjBGVi4u" },
-    { "name": "OI (G7) - Decontamination", "url": "https://forms.cloud.microsoft/Pages/ResponsePage.aspx?id=pX8_-1gNx0GqJPcLmyIgxnmVztZMX6FPrvJPWanW91RUQUwwMk1UMk40MUkyWVlUNUxPNkU0OVdEWC4u" }
-]
-
-const enabledSkills = [
-    "OI (IS1) - Operational Safety (C)",
-    "OI (H6-1) - Bulk Flammable Gases",
-    "OI (H5) - Bulk Flammable Liquids",
-    "OI (FL2-1) - Use of FENZ Operational Vehicles",
-    "OI (E3-2) - Breathing Apparatus (C)",
-    "OI (G9) - Salvage",
-    "OSH - Safe Person Concept (C)",
-    "OI (H1-H6) - Hazardous Materials",
-    "OI (H6-2) - Portable gas cylinders and pressurised vessels",
-    "OI (G2-1) - Emergency Medical Support",
-    "OI (IS3) - Working Near Roadways (C)",
-    "OI (IS4) - Working Near Electrical Hazards (C)",
-    "OI (IS9) - Civil Disturbances",
-    "OI (H7-1) - Clandestine Labs",
-    "OI (G7) - Decontamination"
-]
+// Import constants from the new resource file
+const { members, skillUrls, enabledSkills } = require('./resources.js');
 
 let osmData = [];
 
 async function getOIData() {
     console.log('Retreiving OI Data...');
-    const url = 'https://www.dashboardlive.nz/osm.php?bu=%7b1527694B-2642-4CEC-B9A8-773CA7B1B6CF%7d';
-    // const url = 'http://testutil.work.net.au:8080/dashboard/temp.html';
+    
 
     try {
-        const response = await axios.get(url);
-        const $ = cheerio.load(response.data);
+     
+        const response = await axios.get(url, { httpsAgent: new https.Agent({rejectUnauthorized: false })
+        });
+            
+         const $ = cheerio.load(response.data);
 
         // Select all <tbody> tags
         const osmStatusTable = $('tbody');
@@ -101,7 +48,7 @@ async function getOIData() {
     }
 }
 
-async function sendWhatsApp(member) {
+async function sendMessage(member) {
     let enableSend = false;
     console.log(`Sending message to ${member.name}...`);
     let message = 'Hello, you have expiring Skills due in OSM. Please complete these ASAP.\r\n'
@@ -117,7 +64,7 @@ async function sendWhatsApp(member) {
     if (enableSend){
         console.log('Sending')
         
-        //await sendMessage(member.mobile, message)
+        //await sendEmail(member.email, message)
         .then(() => {
             console.log(`Message sent to ${member.name}`);
         })
@@ -137,17 +84,15 @@ async function checkExpiringSkills(member) {
         oneMonthLater.setMonth(currentDate.getMonth() + 1);
 
         if (skillExpiryDate <= oneMonthLater) {
-            // if (skillExpiryDate <= oneMonthLater) {
-                const retVal = skillUrls.find(skillUrl => skillUrl.name === skill.skill);
+                 const retVal = skillUrls.find(skillUrl => skillUrl.name === skill.skill);
                 if (retVal) skill.url = retVal.url;
-            // }
             return skill;
         }
     })
 
     if (member.expiringSkills && member.expiringSkills.length > 0) {
         console.log(`Found ${member.expiringSkills.length} Expiring Skill(s) for ${member.name}`);
-        //await sendWhatsApp(member);
+        // await sendEmail(member); 
         
     }
     else console.log(`No Expiring Skills for ${member.name}`);
@@ -162,33 +107,23 @@ async function processOIData(rows) {
     for (const member of members) {
         await checkExpiringSkills(member);  // Wait for checkExpiringSkills to finish
         console.log("Waiting...");
-        await new Promise(resolve => setTimeout(resolve, 15000));  // Wait for 30 seconds
+        await new Promise(resolve => setTimeout(resolve, 15000));  // Wait for 15 seconds
     }
 }
 
 const main = async () => {
     try {
-        console.log('Initializing WhatsApp client...');
-        await initializeWhatsAppClient()
-            .then(async () => {
-                console.log('WhatsApp client initialized successfully!');
-                await getOIData()
-                    .then(async (rows) => {
-                        if (rows) {
-                            await processOIData(rows);
-                        } else {
-                            console.log('No OI Data retrieved.');
-                        }
-                    })
-                    .catch((err) => {
-                        console.error('Error:', err.message);
-                    })
-            })
-            .catch((err) => {
-                console.error('Error:', err.message);
-            })
+        // Directly fetch data without initializing WhatsApp
+        const rows = await getOIData();
+        
+        if (rows) {
+            await processOIData(rows);
+        } else {
+            console.log('No OI Data retrieved.');
+        }
+
     } catch (error) {
-        console.error('Error:', error.message);
+        console.error('Error in main process:', error.message);
     }
 }
 main();
---
+++ resources.js
@@ -0,0 +1,65 @@
+const members = [
+    { "name": "QFF Paulin, N M", "email": "", "skills": [] },
+    { "name": "CFO Sweeting-Shaw, P J", "email": "", "skills": [] },
+    { "name": "SFF Fouche, C D", "email": "", "skills": [] },
+    { "name": "DCFO Milne, D S", "email": "", "skills": [] },
+    { "name": "RFF Collins, S B", "email": "", "skills": [] },
+    { "name": "FF Mason, J D", "email": "", "skills": [] },
+    { "name": "FF Godfrey, D C", "email": "", "skills": [] },
+    { "name": "FF Mollier-Alexander, C", "email": "", "skills": [] },
+    { "name": "SFF Reilly, L B", "email": "", "skills": [] },
+    { "name": "FF Fitzpatrick, R", "email": "", "skills": [] },
+    { "name": "QFF Sparrow, M J", "email": "", "skills": [] },
+    { "name": "OS-FF Iszard, A M", "email": "", "skills": [] },
+    { "name": "SO Laloli, B J", "email": "", "skills": [] },
+    { "name": "FF Hunter, W R", "email": "", "skills": [] },
+    { "name": "RFF Hyde, C L", "email": "", "skills": [] },
+    { "name": "RFF Walmsley, B G", "email": "", "skills": [] },
+    { "name": "FF Crawley, P J", "email": "", "skills": [] },
+    { "name": "OS-FF Sweeting-Shaw, B", "email": "", "skills": [] },
+    { "name": "OS-FF Partington, A C", "email": "", "skills": [] }
+];
+
+const skillUrls = [
+    { "name": "OI (IS1) - Operational Safety (C)", "url": "https://forms.office.com/Pages/ResponsePage.aspx?id=pX8_-1gNx0GqJPcLmyIgxnmVztZMX6FPrvJPWanW91RUM0VBMVBDU1VXUTI2RlZNSDM2RDZPVFlUMS4u" },
+    { "name": "OI (H6-1) - Bulk Flammable Gases", "url": "https://forms.office.com/Pages/ResponsePage.aspx?id=pX8_-1gNx0GqJPcLmyIgxnmVztZMX6FPrvJPWanW91RUM0paSDJGWU45NFhFWllLSFROQzZBS0k4TC4u" },
+    { "name": "OI (H5) - Bulk Flammable Liquids", "url": "https://forms.office.com/Pages/ResponsePage.aspx?id=pX8_-1gNx0GqJPcLmyIgxnmVztZMX6FPrvJPWanW91RUQk1ROVVLV0U2T0xMRlJaRkpDQkhMQUtaMy4u" },
+    { "name": "OI (FL2-1) - Use of FENZ Operational Vehicles", "url": "https://forms.office.com/Pages/ResponsePage.aspx?id=pX8_-1gNx0GqJPcLmyIgxnmVztZMX6FPrvJPWanW91RUMlBCS1ZIRUk3QVM0SElPQVZCN1oxWTdBNi4u" },
+    { "name": "OI (E3-2) - Breathing Apparatus (C)", "url": "https://forms.office.com/Pages/ResponsePage.aspx?id=pX8_-1gNx0GqJPcLmyIgxnmVztZMX6FPrvJPWanW91RUNzJMMlQ0Vk1MQkdYWjhQQlFFUEg5SjdDSC4u" },
+    { "name": "OI (G9) - Salvage", "url": "https://forms.office.com/Pages/ResponsePage.aspx?id=pX8_-1gNx0GqJPcLmyIgxnmVztZMX6FPrvJPWanW91RUN0FQN0lJODMxT1NaSVIzSThMUzZYTkxBNy4u" },
+    { "name": "OSH - Safe Person Concept (C)", "url": "https://forms.office.com/Pages/ResponsePage.aspx?id=pX8_-1gNx0GqJPcLmyIgxnmVztZMX6FPrvJPWanW91RURFlDMUY5WURSN0tVMDROTVJYTkMxVTdCNS4u" },
+    { "name": "OI (H1-H6) - Hazardous Materials", "url": "https://forms.office.com/Pages/ResponsePage.aspx?id=pX8_-1gNx0GqJPcLmyIgxnmVztZMX6FPrvJPWanW91RUM1FYRlZDV1FVRUpQNks4WDZDVThKOFEzWi4u" },
+    { "name": "OI (H6-2) - Portable gas cylinders and pressurised vessels", "url": "https://forms.office.com/Pages/ResponsePage.aspx?id=pX8_-1gNx0GqJPcLmyIgxnmVztZMX6FPrvJPWanW91RUN0VDUjNaOTNTWU03WEhYMUJaS0I5NUJXOC4u" },
+    { "name": "OI (G2-1) - Emergency Medical Support", "url": "https://forms.office.com/Pages/ResponsePage.aspx?id=pX8_-1gNx0GqJPcLmyIgxnmVztZMX6FPrvJPWanW91RUMEg2UUQ0VkxYME5RNEUwUTNLQ0ZBSVRNVy4u" },
+    { "name": "OI (IS3) - Working Near Roadways (C)", "url": "https://forms.office.com/Pages/ResponsePage.aspx?id=pX8_-1gNx0GqJPcLmyIgxnmVztZMX6FPrvJPWanW91RUNjdIV0lROTRaTEhFTUFFVUdGQjlSQUhRNC4u" },
+    { "name": "OI (IS4) - Working Near Electrical Hazards (C)", "url": "https://forms.office.com/Pages/ResponsePage.aspx?id=pX8_-1gNx0GqJPcLmyIgxnmVztZMX6FPrvJPWanW91RUQVFUUTJBRkdSNUxSNFBXUjQzWDlCVlkxVC4u" },
+    { "name": "OI (IS9) - Civil Disturbances", "url": "https://forms.office.com/Pages/ResponsePage.aspx?id=pX8_-1gNx0GqJPcLmyIgxnmVztZMX6FPrvJPWanW91RUOExPWlhUSUZHNjQzUFkwVlJFWEJTSE5DWC4u" },
+    { "name": "OI (H7-1) - Clandestine Labs", "url": "https://forms.office.com/Pages/ResponsePage.aspx?id=pX8_-1gNx0GqJPcLmyIgxnmVztZMX6FPrvJPWanW91RUN0RWQjRTU0FXQTFVTDRTWFJOVFRWTjBGVi4u" },
+    { "name": "OI (G7) - Decontamination", "url": "https://forms.cloud.microsoft/Pages/ResponsePage.aspx?id=pX8_-1gNx0GqJPcLmyIgxnmVztZMX6FPrvJPWanW91RUQUwwMk1UMk40MUkyWVlUNUxPNkU0OVdEWC4u" }
+];
+
+const enabledSkills = [
+    "OI (IS1) - Operational Safety (C)",
+    "OI (H6-1) - Bulk Flammable Gases",
+    "OI (H5) - Bulk Flammable Liquids",
+    "OI (FL2-1) - Use of FENZ Operational Vehicles",
+    "OI (E3-2) - Breathing Apparatus (C)",
+    "OI (G9) - Salvage",
+    "OSH - Safe Person Concept (C)",
+    "OI (H1-H6) - Hazardous Materials",
+    "OI (H6-2) - Portable gas cylinders and pressurised vessels",
+    "OI (G2-1) - Emergency Medical Support",
+    "OI (IS3) - Working Near Roadways (C)",
+    "OI (IS4) - Working Near Electrical Hazards (C)",
+    "OI (IS9) - Civil Disturbances",
+    "OI (H7-1) - Clandestine Labs",
+    "OI (G7) - Decontamination"
+];
+const url = 'https://www.dashboardlive.nz/osm.php?bu=%7b1527694B-2642-4CEC-B9A8-773CA7B1B6CF%7d';
+
+module.exports = {
+    members,
+    skillUrls,
+    enabledSkills,
+    url
+};
