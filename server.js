59a6e6e40e73d2cc48467f7985dfee1627c340a7
Gerry Dassi <dassige@gmail.com>
2026-02-03 15:46:47 +1300

    - fixes for MFA

--- server.js
+++ server.js
@@ -99,6 +99,7 @@ app.use((req, res, next) => {
     "/forgot-password",
     "/styles.css",
     "/ui-config",
+    "/login/mfa",
     "/api/demo-credentials",
     "/forms-view.html",
     "/theme.js", // [FIX] Allow security-related frontend scripts
